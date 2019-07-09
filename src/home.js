f6ecdfbf8226f65b905b7143245e1ce200b57b7f
keke7744 <41632496+keke7744@users.noreply.github.com>
2019-07-09 15:45:07 +0100

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+
+const Home = (home)=>{
+  return (<div>
+    <h1>Home page</h1>
+    <h2> Go to my profile</h2>
+  </div>)
+}
+export default Home;
---
+++ src/projects.js
@@ -0,0 +1,14 @@
+class App extends React.Component{
+    render(){
+    
+      return (
+        
+        <div className="profile">
+         <h1>profile</h1>
+        </div>
+        
+      );
+    }
+    }
+    export default Projectss;
+    
